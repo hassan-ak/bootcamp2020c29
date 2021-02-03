5a643ed45531f1d99036186cdc88016e8dfe0e76
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-04 01:20:48 +0500

    Step 04 (create a component and its story)
    1:- Counter.js to create a counter component a simple one
    2:- Counter.stories.js for creating story of the counter

---
+++ cdd-storybooks/src/components/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Counter = () => {
+    return (
+        <div>
+            Hello Counter
+        </div>
+    )
+}
+
+export default Counter;
---
+++ cdd-storybooks/src/stories/Counter.stories.js
@@ -0,0 +1,13 @@
+import Counter from "../components/Counter";
+
+export default {
+    title : "Counter",
+    component : Counter
+}
+
+export function CounterDefault(){
+    return(
+        <Counter/>
+    )
+
+}
