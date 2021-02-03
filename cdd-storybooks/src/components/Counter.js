de88f525f0149d04f09b5342430966f9bf935ffd
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-04 02:08:51 +0500

    Update Counter.js

--- cdd-storybooks/src/components/Counter.js
+++ cdd-storybooks/src/components/Counter.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 
 const Counter = (props) => {
+    const [value,setValue] = useState(props.initialValue)
     return (
         <div style={{backgroundColor:props.backgroundColor}}>
             Hello Counter {props.initialValue}
+            Value {value}
             <br/>
-            <Button title={"increment"} size="small" onClick={()=>{alert("increment")}}/>
-            <Button title={"decrement"} size="large" onClick={()=>{alert("decrement")}}/>
+            <Button title={"increment"} size="small" onClick={()=>{setValue(value+1)}}/>
+            <Button title={"decrement"} size="large" onClick={()=>{setValue(value-1)}}/>
         
         </div>
     )
